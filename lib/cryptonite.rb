3e64d5ba22b46dae74522c8ffbf068c081c9d511
Nikolaos Anastopoulos <ebababi@ebababi.net>
2015-01-15 17:22:27 +0200

    Module refactoring to comply with Rubocop checks

--- lib/cryptonite.rb
+++ lib/cryptonite.rb
@@ -23,17 +23,10 @@ module Cryptonite
     def attr_encrypted(*attributes)
       options = attributes.extract_options!
 
-      @public_key = get_rsa_key(
-        options[:public_key] || options[:key_pair] || ENV['PUBLIC_KEY']
-      )
-      @private_key = get_rsa_key(
-        options[:private_key] || options[:key_pair] || ENV['PRIVATE_KEY'],
-        options[:private_key_password] || ENV['PRIVATE_KEY_PASSWORD']
-      )
+      public_key = extract_public_key(options)
+      private_key = extract_private_key(options)
 
-      attributes.each do |attribute|
-        serialize attribute, Coder.new(@private_key || @public_key)
-      end
+      serialize_attributes_with_coder(attributes, private_key || public_key)
 
       self._attr_encrypted = Set.new(attributes.map(&:to_s)) + (_attr_encrypted || [])
     end
@@ -45,31 +38,56 @@ module Cryptonite
 
     private
 
+    # Serializes all attributes with encryption coder.
+    def serialize_attributes_with_coder(attributes, key)
+      attributes.each do |attribute|
+        serialize attribute, Coder.new(key)
+      end
+    end
+
+    # Extracts public key from options or the environment.
+    def extract_public_key(options)
+      extract_key(options[:public_key] || options[:key_pair] || ENV['PUBLIC_KEY'])
+    end
+
+    # Extracts private key from options or the environment.
+    def extract_private_key(options)
+      extract_key(
+        options[:private_key] || options[:key_pair] || ENV['PRIVATE_KEY'],
+        options[:private_key_password] || ENV['PRIVATE_KEY_PASSWORD']
+      )
+    end
+
     # Retrives an RSA key with multiple ways.
-    def get_rsa_key(key, password = nil)
+    def extract_key(key, password = nil)
       return nil unless key
 
-      if key.is_a?(Proc)
-        key = key.call
-      end
-
-      if key.is_a?(Symbol)
-        key = @instance.send(key)
-      end
-
-      return key if key.is_a?(::OpenSSL::PKey::RSA)
-
-      if key.respond_to?(:read)
-        key = key.read
-      elsif key !~ /^-+BEGIN .* KEY-+$/
-        key = File.read(key)
-      end
-
-      if password.nil?
-        ::OpenSSL::PKey::RSA.new(key)
+      case key
+      when Proc then extract_key_from_proc(key, password)
+      when Symbol then extract_key_from_method(key, password)
+      when ::OpenSSL::PKey::RSA then key
       else
+        key = retrieve_key_string_from_stream(key)
+        return ::OpenSSL::PKey::RSA.new(key) if password.nil?
         ::OpenSSL::PKey::RSA.new(key, password.to_s)
       end
+    end
+
+    # Retrives an RSA key with a `proc` block.
+    def extract_key_from_proc(proc, password = nil)
+      extract_key(proc.call, password)
+    end
+
+    # Retrives an RSA key with a method symbol.
+    def extract_key_from_method(method, password = nil)
+      extract_key(@instance.send(method), password)
+    end
+
+    # Retrives a key string from a stream.
+    def retrieve_key_string_from_stream(stream)
+      return stream.read if stream.respond_to?(:read)
+      return File.read(stream) if stream.to_s !~ /^-+BEGIN .* KEY-+$/
+      stream
     end
   end
 
